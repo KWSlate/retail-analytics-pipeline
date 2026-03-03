e4781f1c513ead5a26dc5ded48d4fdb6ec03e9a8
Ken Slate <kwslate@bellsouth.net>
2026-03-03 14:47:06 -0500

    Update 01.1_create_stg_dv_raw_data_uk_2013.sql

--- SQL/01.1_create_stg_dv_raw_data_uk_2013.sql
+++ SQL/01.1_create_stg_dv_raw_data_uk_2013.sql
@@ -12,6 +12,8 @@ CREATE TABLE [stg].[DV_Raw_Data_UK_2013](
 	[DAY_DT] [datetime] NULL,
 	[DAY_MAIN_DSC] [date] NULL,
 	[RET_STORE_NBR_ID] [varchar](25) NULL,
+	[WM_DEPT_TXT] [varchar](25) NULL,
+	[WM_ACCT_DEPT_NBR_ID] [varchar](25) NULL,
 	[UPC_TXT] [varchar](50) NULL,
 	[WJXBFS1] [decimal](18, 4) NULL,
 	[WJXBFS2] [decimal](18, 4) NULL,
